beb6748b03cbc398ad48afa20b4fe0400d916378
eltobrgs <145704868+eltobrgs@users.noreply.github.com>
2024-02-19 08:22:25 -0300

    Add files via upload

---
+++ desafios de python/desafio10.py
@@ -0,0 +1,5 @@
+#faça um codigo que pergunte quantos reais voce tem e te entregue quantos dolares voce consegue comprar com essa quantia
+ 
+real=  (input('R$ em sua carteira:'))
+dol= real/5.25
+print(f'Você tem {dol} dolares!')
---
+++ desafios de python/desafio3.py
@@ -0,0 +1,6 @@
+#crie um script que leia dois numeros e tente mostrar a soma entre eles 
+
+n1=int(input('Digite um numero qualquer:'))
+n2=int(input('Digite outro numero qualquer:'))
+nf= n1+n2
+print(f'O resultado da soma entre {n1} e {n2} é {nf}')
