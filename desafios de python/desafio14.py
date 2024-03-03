7a0c68c1b8289a3439326adb7f9be05908542da4
eltobrgs <145704868+eltobrgs@users.noreply.github.com>
2024-03-03 18:33:33 -0300

    Delete desafios de python directory

--- desafios de python/desafio14.py
+++
@@ -1,4 +0,0 @@
-#faça um algoritimo que converta graus celsius em farenheith 
-c= float(input("Informe a temperatura em graus celsius que deseja converter:"))
-f = (c * 1.8 + 32)
-print(f"A temperatura {c} em graus celsius corresponde à {f} graus farenheith")
--- desafios de python/desafio18.py
+++
@@ -1,9 +0,0 @@
-#faça um programa que leia um angulo qualquer e mostre na tela seu seno cosseno e tangente 
-import math
-ang= float(input("Digite o algulo em graus:"))
-#a tag math.radians(ang) transforma graus em radianos, fazemos isso pois as tags math.sin, math.cos e math.tg leem radianos e nao graus 
-sen= math.sin(math.radians(ang))
-cos= math.cos(math.radians(ang))
-tg= math.tan(math.radians(ang))
-
-print(f"Quando o angulo é {ang}, seu seno será {sen:.2f}, seu cosseno {cos:.2f} e sua tangente {tg:.2f}")
